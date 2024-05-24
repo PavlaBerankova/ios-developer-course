b290755d58cd9bd1083158c21596bc690a074031
Pavla_Beránková <berankovap@hotmail.com>
2024-05-24 16:10:39 +0200

    Add showInfoAlert method in MainTabBarCoordinator

--- CourseApp/CourseApp/Navigation/MainTabBarCoordinator.swift
+++ CourseApp/CourseApp/Navigation/MainTabBarCoordinator.swift
@@ -37,6 +37,19 @@ private extension MainTabBarCoordinator {
         UITabBar.appearance().standardAppearance = tabBarAppearance
     }
 
+    func setupNavigationBarAppearance(navigationController: UINavigationController, navigationBarTitle: String) {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .accent
+        appearance.shadowImage = UIImage()
+        appearance.titleTextAttributes = [.foregroundColor: TextType.navTitle.uiColor]
+        appearance.largeTitleTextAttributes = [.foregroundColor: TextType.navTitle.uiColor]
+        navigationController.topViewController?.title = navigationBarTitle
+
+        navigationController.navigationBar.standardAppearance = appearance
+        navigationController.navigationBar.compactAppearance = appearance
+        navigationController.navigationBar.scrollEdgeAppearance = appearance
+    }
+
     func makeTabBarController() -> UITabBarController {
         let tabBarController = UITabBarController()
         tabBarController.delegate = self
@@ -61,21 +74,25 @@ private extension MainTabBarCoordinator {
 extension MainTabBarCoordinator: UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
         if viewController === tabBarController.viewControllers?.last {
+            // rootViewController.showInfoAlert(title: "Something is wrong...")
         }
     }
 }
 
-extension MainTabBarCoordinator {
-    func setupNavigationBarAppearance(navigationController: UINavigationController, navigationBarTitle: String) {
-        let appearance = UINavigationBarAppearance()
-        appearance.backgroundColor = .accent
-        appearance.shadowImage = UIImage()
-        appearance.titleTextAttributes = [.foregroundColor: TextType.navTitle.uiColor]
-        appearance.largeTitleTextAttributes = [.foregroundColor: TextType.navTitle.uiColor]
-        navigationController.topViewController?.title = navigationBarTitle
+extension UIViewController {
+    func showInfoAlert(title: String, message: String? = nil, handler: (() -> Void)? = nil) {
+        guard presentedViewController == nil else {
+            return
+        }
 
-        navigationController.navigationBar.standardAppearance = appearance
-        navigationController.navigationBar.compactAppearance = appearance
-        navigationController.navigationBar.scrollEdgeAppearance = appearance
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default
+        ) { _ in
+            handler?()
+        }
+        alertController.addAction(okAction)
+        present(alertController, animated: true)
     }
 }
