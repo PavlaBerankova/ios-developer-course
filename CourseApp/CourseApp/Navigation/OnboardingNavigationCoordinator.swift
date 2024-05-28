f27c2617cbf80e5d31e452977b91931904b45f1b
Pavla_Beránková <berankovap@hotmail.com>
2024-05-28 15:20:27 +0200

    Add OnboardingNavigationCoordinator

--- CourseApp/CourseApp/Navigation/OnboardingNavigationCoordinator.swift
+++ CourseApp/CourseApp/Navigation/OnboardingNavigationCoordinator.swift
@@ -5,4 +5,55 @@
 //  Created by Pavla Beránková on 28.05.2024.
 //
 
-import Foundation
+import os
+import SwiftUI
+import UIKit
+
+protocol OnboardingCoordinating: NavigationControllerCoordinator { }
+
+final class OnboardingNavigationCoordinator: OnboardingCoordinating {
+    let logger = Logger()
+
+    deinit {
+        logger.info("Deinit OnboardingNavigationCoordinator")
+    }
+
+    private(set) lazy var navigationController: UINavigationController = makeNavigationController()
+//    private var cancellables = Set<AnyCancellable>()
+//    private let eventSubject = PassthroughSubject<OnboardingNavigationEvent, Never>()
+    var childCoordinators: [any Coordinator] = []
+}
+
+extension OnboardingNavigationCoordinator {
+    func makeNavigationController() -> UINavigationController {
+        let navigationController = UINavigationController()
+//        navigationController.eventPublisher.sink { [weak self] _ in
+//                   guard let self else {
+//                       return
+//                   }
+//                   self.eventSubject.send(.dismiss(self))
+//               }
+//               .store(in: &cancellables)
+//               navigationController.modalPresentationStyle = .fullScreen
+//               navigationController.modalTransitionStyle = .crossDissolve
+        return navigationController
+    }
+
+    func start() {
+        navigationController.setViewControllers(
+            [makeOnboardingView(), makeOnboardingView(), makeOnboardingView()],
+            animated: false
+        )
+    }
+}
+
+extension OnboardingCoordinating {
+    func makeOnboardingView() -> UIViewController {
+        let controller = UIHostingController(rootView: OnboardingView())
+
+        controller.modalPresentationStyle = .fullScreen
+        controller.modalTransitionStyle = .crossDissolve
+
+        return controller
+    }
+}
